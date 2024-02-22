8ef1d18710c74af3413600ce0341132d3b13c20c
jmailey <johnmmailey@gmail.com>
2024-02-22 22:13:49 +0000

    merge media object into payload

--- src/api/index.js
+++ src/api/index.js
@@ -10,6 +10,15 @@ export default async function getData() {
   const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
   const vehicleDetailsResponse = await Promise
     .allSettled(apiUrls.map((url) => request(url)));
-  const vehicleSummaryPayload = vehicleDetailsResponse.filter(isFulfilledWithValidValues).map(({ value }) => (value));
+
+  const vehicleDetails = vehicleDetailsResponse
+    .filter(isFulfilledWithValidValues)
+    .map(({ value }) => (value));
+
+  const mergeMedia = (details) => vehicles
+    .filter((vehicle) => vehicle.id === details.id)
+    .map((vehicle) => ({ mobile: vehicle.media[1], desktop: vehicle.media[0] }))[0];
+
+  const vehicleSummaryPayload = vehicleDetails.map((details) => ({ ...details, media: mergeMedia(details) }));
   return vehicleSummaryPayload;
 }
