a4851bb87a874f2e48572584fac244508c6cb516
jmailey <johnmmailey@gmail.com>
2024-02-22 19:20:41 +0000

    use vehicle component

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import useData from './useData';
 import './style.scss';
+import Vehicle from '../Vehicle';
 
 export default function VehicleList() {
-  // eslint-disable-next-line no-unused-vars
   const [loading, error, vehicles] = useData();
 
   if (loading) {
@@ -11,30 +11,12 @@ export default function VehicleList() {
   }
 
   if (error) {
-    return <div data-testid="error">{ error }</div>;
+    return <div data-testid="error">{error}</div>;
   }
 
   return (
-    <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
-      <p>
-        Visit
-        <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
-        {' '}
-        (main endpoint)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_fpace.json" target="_blank">/api/vehicle_fpace.json</a>
-        {' '}
-        (detail endpoint - apiUrl)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_xf.json" target="_blank">/api/vehicle_xf.json</a>
-        {' '}
-        (vehicle without any price)
-      </p>
+    <div data-testid="results" className="VehicleList">
+      {vehicles.map((vehicle) => (<Vehicle vehicle={vehicle} key={vehicle.id} />))}
     </div>
   );
 }
