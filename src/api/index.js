959776d5215d7ecb762a0328daec7af4b2645f43
jmailey <johnmmailey@gmail.com>
2024-02-22 12:43:03 +0000

    fetch data

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
-// eslint-disable-next-line no-unused-vars
-import { request } from './helpers';
+import { request, isFulfilledWithValidValues } from './helpers';
 
 /**
  * Pull vehicles information
  *
  * @return {Promise<Array.<vehicleSummaryPayload>>}
  */
-// TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  const vehicles = await request('/api/vehicles.json');
+  const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
+  const vehicleDetailsResponse = await Promise
+    .allSettled(apiUrls.map((url) => request(url)));
+  const vehicleSummaryPayload = vehicleDetailsResponse.filter(isFulfilledWithValidValues).map(({ value }) => (value));
+  return vehicleSummaryPayload;
 }
