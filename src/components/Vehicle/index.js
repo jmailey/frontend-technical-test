efd8edfbd29972665645346502af6351ce9f1249
jmailey <johnmmailey@gmail.com>
2024-02-23 00:27:03 +0000

    add test-ids

--- src/components/Vehicle/index.js
+++ src/components/Vehicle/index.js
@@ -6,16 +6,16 @@ export default function Vehicle({ vehicle }) {
     <div data-testid="vehicle" className="Vehicle">
       <picture className="Vehicle__image">
         <source media="(min-width:768px)" srcSet={vehicle.media.desktop.url} />
-        <img src={vehicle.media.mobile.url} alt={`${vehicle.id} driving on road.`} />
+        <img src={vehicle.media.mobile.url} alt={`${vehicle.id}`} />
       </picture>
       <div className="Vehicle__body">
         <dl className="Vehicle__details">
-          <dt className="Vehicle__detail-label">ID</dt>
-          <dd className="Vehicle__detail-name">{vehicle.id}</dd>
+          <dt className="Vehicle__detail-label">Name</dt>
+          <dd className="Vehicle__detail-name" data-testid="vehicle-id">{vehicle.id}</dd>
           <dt className="Vehicle__detail-label">Price</dt>
-          <dd className="Vehicle__detail-price">{`From ${vehicle.price}`}</dd>
+          <dd className="Vehicle__detail-price" data-testid="vehicle-price">{`From ${vehicle.price}`}</dd>
           <dt className="Vehicle__detail-label">Description</dt>
-          <dd className="Vehicle__detail-description">{vehicle.description}</dd>
+          <dd className="Vehicle__detail-description" data-testid="vehicle-description">{vehicle.description}</dd>
         </dl>
       </div>
     </div>
