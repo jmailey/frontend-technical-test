6a426044a2609531a6992529382bdb2fa4c56097
jmailey <johnmmailey@gmail.com>
2024-02-24 17:37:16 +0000

    remove useless catch

--- src/api/index.js
+++ src/api/index.js
@@ -6,30 +6,25 @@ import { request, isFulfilledWithValidValues } from './helpers';
  * @return {Promise<Array.<vehicleSummaryPayload>>}
  */
 export default async function getData() {
-  try {
-    const vehicles = await request('/api/vehicles.json');
-    const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
-    const vehicleDetailsResponse = await Promise
-      .allSettled(apiUrls.map((url) => request(url)));
+  const vehicles = await request('/api/vehicles.json');
+  const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
+  const vehicleDetailsResponse = await Promise
+    .allSettled(apiUrls.map((url) => request(url)));
 
-    const mergeMedia = (value) => {
-      const matchingVehicle = vehicles.find((vehicle) => vehicle.id === value.id);
-      if (!matchingVehicle || !matchingVehicle.media?.[0] || !matchingVehicle.media?.[1]) {
-        return null;
-      }
-      return {
-        mobile: matchingVehicle.media[1],
-        desktop: matchingVehicle.media[0],
-      };
+  const mergeMedia = (value) => {
+    const matchingVehicle = vehicles.find((vehicle) => vehicle.id === value.id);
+    if (!matchingVehicle || !matchingVehicle.media?.[0] || !matchingVehicle.media?.[1]) {
+      return null;
+    }
+    return {
+      mobile: matchingVehicle.media[1],
+      desktop: matchingVehicle.media[0],
     };
+  };
 
-    const vehicleSummaryPayload = vehicleDetailsResponse
-      .filter(isFulfilledWithValidValues)
-      .map(({ value }) => ({ ...value, media: mergeMedia(value) }));
+  const vehicleSummaryPayload = vehicleDetailsResponse
+    .filter(isFulfilledWithValidValues)
+    .map(({ value }) => ({ ...value, media: mergeMedia(value) }));
 
-    return vehicleSummaryPayload;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  return vehicleSummaryPayload;
 }
