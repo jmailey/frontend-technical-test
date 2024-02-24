629aae2d9a385d6c36da89cf7c846b95c218bae2
jmailey <johnmmailey@gmail.com>
2024-02-24 19:43:48 +0000

    test hook

---
+++ src/components/VehicleList/__tests__/useData.test.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { renderHook, act } from '@testing-library/react-hooks';
+import useData from '../useData';
+import getData from '../../../api/index';
+
+jest.mock('../../../api/index', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('useData hook', () => {
+  it('fetches data correctly', async () => {
+    const mockData = [{ id: 1, name: 'Car' }, { id: 2, name: 'Bike' }];
+    getData.mockResolvedValueOnce(mockData);
+
+    await act(async () => {
+      const { result, waitForNextUpdate } = renderHook(() => useData());
+      const [initialLoading] = result.current;
+      expect(initialLoading).toBe(true);
+
+      await waitForNextUpdate();
+      const [loading, error, vehicles] = result.current;
+
+      expect(loading).toBe(false);
+      expect(error).toBe('');
+      expect(vehicles).toEqual(mockData);
+    });
+  });
+
+  it('handles error correctly', async () => {
+    const mockError = 'Failed to fetch data';
+    getData.mockRejectedValueOnce(mockError);
+    await act(async () => {
+      const { result, waitForNextUpdate } = renderHook(() => useData());
+      const [initialLoading] = result.current;
+      expect(initialLoading).toBe(true);
+
+      await waitForNextUpdate();
+      const [loading, error, vehicles] = result.current;
+      expect(loading).toBe(false);
+      expect(error).toBe(mockError);
+      expect(vehicles).toEqual([]);
+    });
+  });
+});
